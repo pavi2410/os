d8935e5e4bbaa60b7f63ade35043d5a9907eb764
pavi2410 <hello@pavi2410.me>
2025-07-05 15:16:33 +0530

    Refactor memory map handling and enhance output formatting in main function

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,64 @@
 const std = @import("std");
 const uefi = std.os.uefi;
 const fmt = std.fmt;
-
+const MemoryDescriptor = uefi.tables.MemoryDescriptor;
+const MemoryType = uefi.tables.MemoryType;
 const W = std.unicode.utf8ToUtf16LeStringLiteral;
+
+const banner =
+    \\               ,-.    ,.  ,  ,-.      /         
+    \\             o    )  / | '| /  /\    /          
+    \\ ;-. ,-: . , .   /  '--|  | | / |   /   ,-. ,-. 
+    \\ | | | | |/  |  /      |  | \/  /  /    | | `-. 
+    \\ |-' `-` '   ' '--'    '  '  `-'  /     `-' `-' 
+    \\ '                                              
+;
+
+const DEBUG = false;
 
 pub fn main() void {
     const con_out = uefi.system_table.con_out.?;
+    const boot_services = uefi.system_table.boot_services.?;
+
     _ = con_out.clearScreen();
 
-    printf("Hello\r\nyeah\r\n", .{});
+    printMultine(banner);
 
-    displayMemoryMap();
+    const mmap = getMemoryMap().?;
 
-    while (true) {}
+    if (DEBUG) {
+        printMemoryMap(mmap.mmap_ptr, mmap.mmap_size, mmap.desc_size, mmap.desc_count);
+    }
+
+    const status = boot_services.exitBootServices(uefi.handle, mmap.map_key);
+    if (status != .success) {
+        printf("ExitBootServices failed: {}\r\n", .{status});
+        while (true) {}
+    }
+
+    printf("ExitBootServices succeeded.\r\n", .{});
+
+    // cannot call UEFI after this point!
+    jumpToKernel(mmap.mmap_ptr, mmap.mmap_size, mmap.desc_size, mmap.desc_count);
 }
 
-pub fn printf(comptime format: [:0]const u8, args: anytype) void {
+fn jumpToKernel(
+    mem_map: [*]align(8) u8,
+    mem_map_size: usize,
+    desc_size: usize,
+    desc_count: usize,
+) noreturn {
+    _ = mem_map; // Use the memory map pointer if needed
+    _ = mem_map_size; // Use the memory map size if needed
+    _ = desc_size; // Use the descriptor size if needed
+    _ = desc_count; // Use the descriptor count if needed
+    // for now just hang
+    while (true) {
+        asm volatile ("hlt");
+    }
+}
+
+fn printf(comptime format: [:0]const u8, args: anytype) void {
     const con_out = uefi.system_table.con_out.?;
 
     var buf8: [256]u8 = undefined;
@@ -27,11 +70,21 @@ pub fn printf(comptime format: [:0]const u8, args: anytype) void {
     _ = con_out.outputString(@ptrCast(buf16[0..]));
 }
 
-fn displayMemoryMap() void {
-    const boot_services = uefi.system_table.boot_services.?;
+fn printMultine(msg: [:0]const u8) void {
+    var splits = std.mem.splitScalar(u8, msg, '\n');
+    while (splits.next()) |chunk| {
+        printf("{s}\r\n", .{chunk});
+    }
+}
 
-    const MemoryDescriptor = uefi.tables.MemoryDescriptor;
-    const MemoryType = uefi.tables.MemoryType;
+fn getMemoryMap() ?struct {
+    mmap_ptr: [*]align(8) u8,
+    mmap_size: usize,
+    map_key: usize,
+    desc_size: usize,
+    desc_count: usize,
+} {
+    const boot_services = uefi.system_table.boot_services.?;
 
     var mmap_size: usize = 0;
     var map_key: usize = 0;
@@ -50,7 +103,7 @@ fn displayMemoryMap() void {
         printf("Failed to retrieve memory map. status = {}\r\n", .{
             status,
         });
-        return;
+        return null;
     }
 
     mmap_size += 2 * desc_size;
@@ -60,7 +113,7 @@ fn displayMemoryMap() void {
     const alloc_status = boot_services.allocatePool(MemoryType.loader_data, mmap_size, @ptrCast(&mmap_ptr));
     if (alloc_status != .success) {
         printf("Failed to allocate memory for memory map. status = {}\r\n", .{alloc_status});
-        return;
+        return null;
     }
 
     const status2 = boot_services.getMemoryMap(
@@ -73,18 +126,26 @@ fn displayMemoryMap() void {
 
     if (status2 != .success) {
         printf("Failed to retrieve memory map. status = {}\r\n", .{status2});
-        return;
+        return null;
     }
-
-    printf("Memory map retrieved successfully.\r\n", .{});
 
     const desc_count = mmap_size / desc_size;
 
+    return .{
+        .mmap_ptr = mmap_ptr,
+        .mmap_size = mmap_size,
+        .map_key = map_key,
+        .desc_size = desc_size,
+        .desc_count = desc_count,
+    };
+}
+
+fn printMemoryMap(mmap_ptr: [*]align(8) u8, mmap_size: usize, desc_size: usize, desc_count: usize) void {
     printf("Memory map size: {d} bytes, Descriptor size: {d} bytes, Descriptor count: {d}\r\n", .{
         mmap_size, desc_size, desc_count,
     });
 
-    printf("MemoryDescriptor size: {d} bytes\r\n", .{ @sizeOf(MemoryDescriptor) });
+    printf("MemoryDescriptor size: {d} bytes\r\n", .{@sizeOf(MemoryDescriptor)});
 
     var b: usize = 0;
     var i: usize = 0;
